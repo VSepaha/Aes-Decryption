187928c056bd237e3eed15342aca4017fbf0eb84
VSepaha <vineetsepaha@gmail.com>
2016-12-04 23:02:40 -0500

    zero padding added

--- aesCracker.c
+++ aesCracker.c
@@ -20,7 +20,7 @@ typedef char string[MAX_CHARS+1];
 int getMaxValue(uint8_t *buffer){
 	int i;
 	int max = 0;
-	for(i = 0; i < 31; i++){
+	for(i = 0; i < 16; i++){
 		int temp = (int)buffer[i];
 		if (max < temp){
 			max = temp;
@@ -29,70 +29,15 @@ int getMaxValue(uint8_t *buffer){
 	return max;
 }
 
-void testDecrypt(uint8_t* key, uint8_t* ciphertext, string *dictionary){
-	printf("Test Case: \n");
-
-  	uint8_t buffer[SIZE];
-  
-  	// Decryption happens here 
-  	// Electronic Code book decrypts 16 bytes at a time 
-  	int i;
-  	for(i = 0; i<(SIZE-1); i+=16){
-  		AES128_ECB_decrypt(ciphertext+i, key, buffer+i);
-  	}
-
-  	//To print out the values in the buffer
-  	// for (i = 0; i < 31; i++){
-  	// 	printf("%d ", buffer[i]);
-  	// }
-  	// printf("\n");
-
-  	int max = getMaxValue(buffer);
-  	//printf("The max value in the buffer is: %d\n", max);
-  	if (max > 122){
-  		printf("Plaintext contains invalid characters\n");
-  		return;
-  	}
-
-  	//Convert to plaintext and print
-  	char *plaintext = (char *)(intptr_t)buffer;
-  	printf("Output Plaintext: %s\n", plaintext);
-
-  	//Change everything to lowercase so it is easier to compare
-  	for(i = 0; i < (SIZE-1); i++){
-  		plaintext[i] = tolower(plaintext[i]);
-	}
-
-
-	//iterate through the dictionary to see if a word is contained in the plaintext
-	int count = 0;
-  	for(i = 0; i < DICTIONARY_LEN; i++){
-  		if(strstr(plaintext, dictionary[i]) != NULL){
-  			//Print all the words contained in the plaintext
-  			printf("Plaintext contains a word '%s'\n", dictionary[i]);
-  			count++;
-  		}
-  		//If no valid words are found
-  		if (i == DICTIONARY_LEN-1 && count == 0){
-  			printf("Plaintext contains no words\n");
-  			return;
-  		}
-  	}
-
-}
-
 int decrypt(uint8_t* key, uint8_t* ciphertext, string *dictionary){
 	int i, count;
 	uint8_t buffer[SIZE];
 
-	for(i = 0; i<(SIZE-1); i+=16){
+	for(i = 0; i<(SIZE); i+=16){
   		AES128_ECB_decrypt(ciphertext+i, key, buffer+i);
-  	}
-  	int max = getMaxValue(buffer);
-
-  	//Return false if out of range ASCII value
-  	if (max > 122){
-  		return false;
+  		int max = getMaxValue(buffer+i);
+  		if (max > 122)
+  			return false;
   	}
 
   	//Save buffer to plaintext 
@@ -104,7 +49,7 @@ int decrypt(uint8_t* key, uint8_t* ciphertext, string *dictionary){
 	count = 0;
 	for(i = 0; i < DICTIONARY_LEN; i++){
   		if(strstr(plaintext, dictionary[i]) != NULL){
-  			printf("Plaintext contains a word '%s'\n", dictionary[i]);
+  			//printf("Plaintext contains a word '%s'\n", dictionary[i]);
   			count++;
   		}
   		//If no valid words are found
@@ -113,8 +58,7 @@ int decrypt(uint8_t* key, uint8_t* ciphertext, string *dictionary){
   		}
   	}
 
-  	char *outputKey = (char *)(intptr_t)key;
-  	printf("%s\n", outputKey);
+  	printf("Plaintext = %s\n", plaintext);
   	return true;
 }
 
@@ -151,6 +95,7 @@ int decrypt(uint8_t* key, uint8_t* ciphertext, string *dictionary){
 // 	}
 // }
 
+
 int main(){
 	
 	//Set the dictionary that we will be using
@@ -164,15 +109,28 @@ int main(){
 	}
 	fclose(myfile);
 
-	uint8_t key[] = {0x6F, 0x1C, 0x5C, 0xD9, 0x27, 0x0A, 0xC8, 0xDD, 0xEA, 0xE6, 0x43, 0x0F, 0x30, 0x96, 0xC8, 0x06};
-  	uint8_t ciphertext[]  = {0x11, 0x37, 0x59, 0x0E, 0x76, 0x02, 0x25, 0x6E, 0x37, 0xFC, 0xD3, 0x68, 0x55, 0xCC, 0x93, 0x53,0xC1, 0xF2, 0xC2, 0x11, 0x71, 0xF2, 0xEC, 0x03, 0x91, 0xBE, 0xEE, 0x9A, 0x0A, 0x19, 0xB0, 0x84};
+	uint8_t initVector[] = {0x6F, 0x1C, 0x5C, 0xD9, 0x27, 0x0A, 0xC8, 0xDD, 0xEA, 0xE6, 0x43, 0x0F, 0x30, 0x96, 0xC8, 0x06};
+  	uint8_t ciphertext[] = {0x11, 0x37, 0x59, 0x0E, 0x76, 0x02, 0x25, 0x6E, 0x37, 0xFC, 0xD3, 0x68, 0x55, 0xCC, 0x93, 0x53,0xC1, 0xF2, 0xC2, 0x11, 0x71, 0xF2, 0xEC, 0x03, 0x91, 0xBE, 0xEE, 0x9A, 0x0A, 0x19, 0xB0, 0x84};
 
-  	//key = key_increment(key);
+  	int IVsize = (int)sizeof(initVector);
+  	printf("Size of IV = %d\n", IVsize);
 
+  	//zero padding incase key is not long enough
+  	uint8_t key[16] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+  	for(i = 0; i < sizeof(initVector); i++){
+	 	key[i] = initVector[i];
+	}
+	
 	//Call the test decrpytion function
+	// while(!check){
+	// 	//increment the key
+	// 	//call decrypt
+	// }
 	check = decrypt(key, ciphertext, dictionary);
 	if(check){
 		printf("Decryption Successful\n");
+	} else {
+		printf("Decryption Failed\n");
 	}
 	return 0;
 }
