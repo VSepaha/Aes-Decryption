254d7dec5f6a2b46e5617b17e9f0fcefbe80bcea
VSepaha <vineetsepaha@gmail.com>
2016-12-05 22:54:03 -0500

    merge

--- aesCracker.c
+++ aesCracker.c
@@ -85,15 +85,17 @@ int main(){
 	fclose(myfile);
 
 	//length of 14 (hex)
-	uint8_t *initializationVector = "639404CBD1A1BD2322B206C39140"; 
+	//uint8_t *initializationVector = "639404CBD1A1BD2322B206C39140"; 
 	//length of 64 (hex)
-	string initCiphertext = "5A052F928464CC3E437187ADCFC7E8F1CF9DEAC7059B5264E4E940D8C35AA60E2277D4832843043F593F40E4084609C886681BCF5B570D353BFF24C0E1F4A65E";
+	//string initCiphertext = "5A052F928464CC3E437187ADCFC7E8F1CF9DEAC7059B5264E4E940D8C35AA60E2277D4832843043F593F40E4084609C886681BCF5B570D353BFF24C0E1F4A65E";
 
-	uint8_t initVector[] = {0x6F, 0x1C, 0x5C, 0xD9, 0x27, 0x0A, 0xC8, 0xDD, 0xEA, 0xE6, 0x43, 0x0F, 0x30, 0x96, 0xC8, 0x06};
+
+	//This thing is missing the last two hex values
+	uint8_t initVector[] = {0x6F, 0x1C, 0x5C, 0xD9, 0x27, 0x0A, 0xC8, 0xDD, 0xEA, 0xE6, 0x43, 0x0F, 0x30, 0x96};
   	uint8_t ciphertext[] = {0x11, 0x37, 0x59, 0x0E, 0x76, 0x02, 0x25, 0x6E, 0x37, 0xFC, 0xD3, 0x68, 0x55, 0xCC, 0x93, 0x53,0xC1, 0xF2, 0xC2, 0x11, 0x71, 0xF2, 0xEC, 0x03, 0x91, 0xBE, 0xEE, 0x9A, 0x0A, 0x19, 0xB0, 0x84};
   	int size = (int)sizeof(ciphertext);
 
-  	printf("%d\n", initializationVector[0]);
+  	//printf("%d\n", initializationVector[0]);
 
   	//zero padding incase key is not long enough
   	uint8_t key[16] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
@@ -107,10 +109,13 @@ int main(){
 
 	i = 0;
 	check = false;
+	int loopCounter = 0;
 	while(!check){
+		loopCounter++;
 		check = decrypt(key, ciphertext, dictionary, size);
 		incrementKey(key);
 	}
+	printf("Loop counter = %d\n", loopCounter);
 
 	return 0;
 }
